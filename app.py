51d96007d8650b4ea99ffbbfae793d204c5fbfed
Garrett Gordon <gagordon@mit.edu>
2022-07-02 12:46:55 -0400

    missing comma

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ if __name__ == "__main__":
     if DEVELOPMENT:
         app.run(debug=True, port=PORT)
     else:
-        app.run(host="0.0.0.0" debug=False, port=PORT)
+        app.run(host="0.0.0.0", debug=False, port=PORT)
