e7fe68ee67fc4d7999eb055d6f26fb60d6ae4a76
Garrett Gordon <gagordon@mit.edu>
2022-07-08 14:21:30 -0400

    removed some debugging prints

--- resources/aws_operations.py
+++ resources/aws_operations.py
@@ -34,9 +34,6 @@ def delete_amazon_bucket_file(access_key, access_id, bucket, key, verbose=False)
     s3 = boto3.resource('s3',
         aws_access_key_id=access_id,
         aws_secret_access_key= access_key)
-    if verbose:
-        print("     " + key +  "...")
-        print("     " + key + "/download.mp3...")
     result = s3.Object(bucket, key).delete()
     result = s3.Object(bucket, key + "/download.mp3").delete() #delete associated downloadable file
 
